5cb2f16b30c56d4849a63cef360f870947256254
Grant Faiks <grant.faiks@axiosengineering.com>
2016-08-29 15:40:58 -0400

    Adding unit tests for checking vita49 stream data. Refs CA-380

--- tests/test_SourceVITA49.py
+++ tests/test_SourceVITA49.py
@@ -19,13 +19,23 @@
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 
-DEBUG_LEVEL = 3
+DEBUG_LEVEL = 5
+LITTLE_ENDIAN=1234
+BIG_ENDIAN=4321
 
 import unittest
 import ossie.utils.testing
 import os
 from omniORB import any
 from ossie.utils import sb
+from ossie.utils.bulkio import bulkio_data_helpers
+from bulkio.bulkioInterfaces import BULKIO, BULKIO__POA
+import bulkio
+from ossie.cf import CF
+from bulkio import timestamp
+import time
+import socket
+from vita49 import VITA49 
 
 # Full functionality is tested via end-to-end testing using SinkVITA49 and SourceVITA49 in the fulltest_VITA49.py file
 class ResourceTests(ossie.utils.testing.ScaComponentTestCase):
@@ -36,8 +46,17 @@ class ResourceTests(ossie.utils.testing.ScaComponentTestCase):
         """Set up the unit test - this is run before every method that starts with test
         """
         ossie.utils.testing.ScaComponentTestCase.setUp(self)
-        
+        self.port = 29495
+        self.uni_ip = '127.0.0.1'
+        self.multi_ip = '236.0.10.1'
+        self.vector_size=512
         self.componentSetup()
+        self.comp.interface = "lo" 
+        self.comp.advanced_configuration.corba_transfersize = 100
+        self.comp.advanced_configuration.buffer_size=100
+        self.input_vita49_port = self.comp.getPort("dataVITA49_in")
+        self.setupSocket()
+        
         
         
     def tearDown(self):
@@ -72,7 +91,129 @@ class ResourceTests(ossie.utils.testing.ScaComponentTestCase):
         
         # SourceVITA49 needs to be configured prior to being started, so it must be started in each unit test.
         #self.comp.start()
+    
+    def connectOutputData(self,portName= "dataShort_out"):
+        self.dataSink = sb.DataSink()       
+        self.comp.connect(self.dataSink,usesPortName=portName)
+     
+    def pushSRI(self,xdelta=1,streamID='TestStreamID',mode=0,kw=[]):
+        kw.append(CF.DataType("dataRef", ossie.properties.to_tc_value(BIG_ENDIAN, 'string')))
+        sri = BULKIO.StreamSRI(hversion=1, xstart=0.0, xdelta=xdelta, xunits=1, subsize=0, ystart=0.0, ydelta=0.0, yunits=0, mode=mode, streamID=streamID, blocking=False, keywords=kw)
+        self.input_vita49_port.pushSRI(sri,timestamp.now())          
+  
+
+    def callAttach(self,valid_data_format=True,mode=0):
+        # Mode 0 = RealShort ,  1 = ComplexShort, 2 = RealFloat, 3 = ComplexFloat
         
+        payloadFormat_RealShort = BULKIO.VITA49DataPacketPayloadFormat(True,BULKIO.VITA49_REAL,BULKIO.VITA49_16T,False,0,0,16,16,1,1)
+        payloadFormat_ComplexShort = BULKIO.VITA49DataPacketPayloadFormat(True,BULKIO.VITA49_COMPLEX_CARTESIAN,BULKIO.VITA49_16T,False,0,0,16,16,1,1)
+        payloadFormat_RealFloat = BULKIO.VITA49DataPacketPayloadFormat(True,BULKIO.VITA49_REAL,BULKIO.VITA49_32F,False,0,0,32,32,1,1)
+        payloadFormat_ComplexFloat = BULKIO.VITA49DataPacketPayloadFormat(True,BULKIO.VITA49_COMPLEX_CARTESIAN,BULKIO.VITA49_32F,False,0,0,32,32,1,1)
+        payloadFormat_RealLong = BULKIO.VITA49DataPacketPayloadFormat(True,BULKIO.VITA49_REAL,BULKIO.VITA49_32T,False,0,0,32,32,1,1)
+        payloadFormat_RealOctet = BULKIO.VITA49DataPacketPayloadFormat(True,BULKIO.VITA49_REAL,BULKIO.VITA49_8T,False,0,0,8,8,1,1)
+        
+        if mode == 0:
+            payloadFormat = payloadFormat_RealShort
+        elif mode ==1:
+            payloadFormat = payloadFormat_ComplexShort
+        elif mode ==2:
+            payloadFormat = payloadFormat_RealFloat
+        elif mode ==3:
+            payloadFormat = payloadFormat_ComplexFloat
+        elif mode ==4:
+            payloadFormat = payloadFormat_RealLong
+        elif mode ==5:
+            payloadFormat = payloadFormat_RealOctet
+        else:
+            payloadFormat = None
+        
+        streamDef = BULKIO.VITA49StreamDefinition(self.uni_ip,0,self.port,BULKIO.VITA49_UDP_TRANSPORT,"id", valid_data_format,payloadFormat)
+                
+        # Try to attach
+        attachId = ''
+        
+        try:
+            attachId = self.input_vita49_port.attach(streamDef, 'test') 
+        except Exception, e:
+            print str(e)
+            print "ATTACH FAILED"
+            attachId = ''
+        
+        self.assertTrue(attachId != '', "Failed to attach to Vita49 component")
+
+        return attachId
+
+    def boosthashStr(self,inputStr):
+      seed = 0
+      for c in inputStr:
+        seed ^= (ord(c) + 0x9e3779b9 + (seed<<6) + (seed>>2)) & 0xffffffffffffffff
+      return seed & 0xffffffff
+  
+    def setupSocket(self):
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self.sock.bind((self.uni_ip, self.port))
+        
+    
+    def writeSocketData(self,dataIn):
+        #dataOut= ""
+        #for i in range(0, len(dataIn), 4):
+            #data = struct.pack("I",dataIn[i:i+3])
+        #    dataOut+= socket.hton1(data) # Switch to network Order (BIG ENDIAN)
+            
+        sent = self.sock.sendto(dataIn,(self.uni_ip, self.port))
+        if sent != len(dataIn):
+                raise RuntimeError("socket Error")
+            
+    def createVITA49Data(self,streamID="vita49TestData",format=VITA49.DATA_FORMATS.SI,bandwidth=20e6,sample_rate=25e6,rf_ref=100e6,vector_size=512 ):
+        v49 = VITA49()
+        v49.timestamp = time.time()
+        v49.streamID = self.boosthashStr(streamID)
+        v49.format = format
+        v49.bandwidth = bandwidth
+        v49.sample_rate = sample_rate
+        if v49.format[0]: # Complex
+            v49.if_ref = 0
+        else: # Real
+            v49.if_ref = v49.sample_rate/4
+        v49.rf_ref = rf_ref
+        v49.vector_size = vector_size
+        
+        return v49
+
+    def sendDataandCheck(self,vita49Frame):
+        
+        Indata=[x %256 for x in range(self.vector_size)]
+
+        # Send Data Packet
+        frame = vita49Frame.generateVRLFrame(Indata, force_send_context=False)       
+        self.writeSocketData(frame)
+        time.sleep(.1)
+        
+        # Send Context Packet
+        frame = vita49Frame.generateVRLFrame(None, force_send_context=True)       
+        self.writeSocketData(frame)
+        time.sleep(.1)
+
+        # Send Data Packets
+        frame = vita49Frame.generateVRLFrame(Indata, force_send_context=False)       
+        self.writeSocketData(frame)
+
+        frame = vita49Frame.generateVRLFrame(Indata, force_send_context=False)       
+        self.writeSocketData(frame)
+
+        frame = vita49Frame.generateVRLFrame(Indata, force_send_context=False)       
+        self.writeSocketData(frame)
+
+         
+        time.sleep(3)
+  
+        data= self.dataSink.getData()
+        if len(data) > 0:
+            self.assertEqual(data[:self.vector_size], Indata[:self.vector_size])
+        else:
+            self.assertTrue(False, "Did not get Data")     
+
         
     def testStartStopRelease(self):
         """testStartStopRelease - Only start, stop, then release the component.
@@ -155,7 +296,7 @@ class ResourceTests(ossie.utils.testing.ScaComponentTestCase):
         """
         #######################################################################
         # Attempt to set all the properties
-        self.comp.interface = "eth0" 
+        self.comp.interface = "lo" 
         self.comp.attachment_override.ip_address = "127.0.0.1"
         self.comp.attachment_override.vlan = 0
         self.comp.attachment_override.use_udp_protocol = True
@@ -183,5 +324,118 @@ class ResourceTests(ossie.utils.testing.ScaComponentTestCase):
         print ' * udp socket and will fail to launch the RX thread.'
         self.comp.start()
 
+    def testAttachDetach(self):
+        
+        self.comp.start()
+        self.pushSRI()
+        attachId = self.callAttach(True,0)
+        self.input_vita49_port.detach(attachId)
+
+    def testSRI(self):
+        
+        self.connectOutputData()
+
+        
+        self.pushSRI(xdelta=.0001,mode=1,streamID="testSRIStream", kw=[CF.DataType("COL_RF", ossie.properties.to_tc_value(100000000, 'double'))])
+        attachId = self.callAttach(True,0)
+        self.comp.start()
+
+        sri = self.dataSink.sri()
+        self.assertEqual(sri.streamID,"testSRIStream")
+        self.assertEqual(sri.xdelta,.0001)
+        self.assertEqual(sri.mode,1)
+        
+        expectedkws = [CF.DataType("dataRef", ossie.properties.to_tc_value(BIG_ENDIAN, 'long')),
+                       CF.DataType("COL_RF", ossie.properties.to_tc_value(100000000, 'double'))]
+        
+        for expectedkw in expectedkws:
+            found = False
+            for keyword in sri.keywords:
+                if expectedkw.id==keyword.id:
+                    found=True
+                    self.assertEqual(expectedkw.value.value(),keyword.value.value())
+                    break
+            self.assertEqual(found,True)
+        
+        
+        #Change SRI and see if changed on output
+        self.pushSRI(xdelta=.01,mode=0,streamID="testSRIStream", kw=[CF.DataType("COL_RF", ossie.properties.to_tc_value(100000000, 'double'))])
+        time.sleep(.5)
+        sri = self.dataSink.sri()
+        self.assertEqual(sri.streamID,"testSRIStream")
+        self.assertEqual(sri.xdelta,.01)
+        self.assertEqual(sri.mode,0)        
+ 
+        # Update just a keyword
+        self.pushSRI(xdelta=.01,mode=0,streamID="testSRIStream", kw=[CF.DataType("COL_RF", ossie.properties.to_tc_value(200000000, 'double'))])
+        time.sleep(.5)
+        sri = self.dataSink.sri()
+ 
+        expectedkws = [CF.DataType("dataRef", ossie.properties.to_tc_value(BIG_ENDIAN, 'long')),
+                       CF.DataType("COL_RF", ossie.properties.to_tc_value(200000000, 'double'))]
+        
+        for expectedkw in expectedkws:
+            found = False
+            for keyword in sri.keywords:
+                if expectedkw.id==keyword.id:
+                    found=True
+                    self.assertEqual(expectedkw.value.value(),keyword.value.value())
+                    break
+            self.assertEqual(found,True)
+        
+        
+        self.input_vita49_port.detach(attachId)        
+
+    def testSendDataSI(self):
+        
+        
+        self.connectOutputData(portName= "dataShort_out")
+
+        attachId = self.callAttach(True,0)
+        self.pushSRI(xdelta=1/10000,mode=1,streamID="testSendDataSI", kw=[CF.DataType("COL_RF", ossie.properties.to_tc_value(100000000, 'double'))])
+
+        self.comp.start()
+        time.sleep(1)
+        
+        v49 = self.createVITA49Data(streamID="testSendDataSI",format=VITA49.DATA_FORMATS.SI,vector_size=self.vector_size)
+        self.sendDataandCheck(v49)
+  
+
+        print "****Send Detach "  
+        self.input_vita49_port.detach(attachId)
+
+    def testSendDataCI(self):
+                
+        self.connectOutputData(portName= "dataShort_out")
+        attachId = self.callAttach(True,1)
+        self.pushSRI(xdelta=1/10000,mode=1,streamID="testSendDataCI", kw=[CF.DataType("COL_RF", ossie.properties.to_tc_value(100000000, 'double'))])
+
+        self.comp.start()
+        time.sleep(1)
+        
+        v49 = self.createVITA49Data(streamID="testSendDataCI",format=VITA49.DATA_FORMATS.CI,vector_size=self.vector_size/2)
+        self.sendDataandCheck(v49)
+  
+
+        print "****Send Detach "  
+        self.input_vita49_port.detach(attachId)    
+        
+    def testSendDataSF(self):
+                
+        self.connectOutputData(portName= "dataFloat_out")
+
+        attachId = self.callAttach(True,2)
+        self.pushSRI(xdelta=1/10000,mode=1,streamID="testSendDataSF", kw=[CF.DataType("COL_RF", ossie.properties.to_tc_value(100000000, 'double'))])
+
+        self.comp.start()
+        time.sleep(1)
+        
+        v49 = self.createVITA49Data(streamID="testSendDataSF",format=VITA49.DATA_FORMATS.SF,vector_size=self.vector_size)
+        self.sendDataandCheck(v49)
+  
+
+        print "****Send Detach "  
+        self.input_vita49_port.detach(attachId)    
+        
 if __name__ == "__main__":
     ossie.utils.testing.main("../SourceVITA49.spd.xml") # By default tests all implementations
---
+++ tests/vita49.py
@@ -0,0 +1,290 @@
+#!/bin/env python
+import math
+import struct
+import time
+import random
+import binascii
+import copy
+
+class VITA49():
+    #Options/Settings
+    timestamp = None
+    streamID = None
+    packet_count = 0
+    #CIF options
+    bandwidth = None
+    sample_rate = None
+    rf_ref = None
+    if_ref = None
+    format = None
+    vector_size = 0
+    
+    #Helper classes
+    class PKT_TYPE:
+        if_data_packet_without_stream_id = 0
+        if_data_packet_with_stream_id = 1
+        extention_data_packet_without_stream_id = 2
+        extention_data_packet_with_stream_id = 3
+        if_context_packet = 4
+        extension_context_packet = 5
+        
+    class TSI:
+        no_timestamp = 0
+        utc_time = 1
+        gps_time = 2
+        other = 3
+        
+    class TSF:
+        no_timestamp = 0
+        sample_count = 1
+        real_time = 2
+        free_running = 3
+        
+    class CIF:
+        no_fields =             0x00000000
+        ctx_field_change =      0x80000000
+        ref_point_id =          0x40000000
+        bandwidth =             0x20000000
+        if_ref_freq =           0x10000000
+        rf_ref_freq =           0x08000000
+        rf_ref_freq_off =       0x04000000
+        if_band_off =           0x02000000
+        ref_lvl =               0x01000000
+        gain =                  0x00800000
+        over_range_count =      0x00400000
+        sample_rate =           0x00200000
+        timestamp_adjust =      0x00100000
+        timestamp_calibrate =   0x00080000
+        temp =                  0x00040000
+        dev_id =                0x00020000
+        state_event_indicate =  0x00010000
+        pkt_payload_fmt =       0x00008000
+        fmt_gps_geo =           0x00004000
+        fmt_ins_geo =           0x00002000
+        ecef_ephemeris =        0x00001000
+        rel_ephemeris =         0x00000800
+        ephemeris_ref_id =      0x00000400
+        gps_ascii =             0x00000200
+        cts_assoc_list =        0x00000100
+        all_fields =            0xFFFFFF00
+    
+    class DATA_FORMATS:
+        #Formats are (complex, bytes_per_sample, V49 Type, ClassID Type)
+        SB = (0, 1, 0, 2, 'b')
+        CB = (1, 1, 0, 2, 'b')
+        SI = (0, 2, 0, 3, 'h')
+        CI = (1, 2, 0, 3, 'h')
+        SL = (0, 4, 0, 4, 'i')
+        CL = (1, 4, 0, 4, 'i')
+        SF = (0, 4, 14, 6, 'f')
+        CF = (1, 4, 14, 6, 'f')
+        SD = (0, 8, 15, 7, 'd')
+        CD = (1, 8, 15, 7, 'd')
+    
+    def generateCommonHeader(self, context=False):
+        pkt = []
+        #Generate header word
+        hdr = 0x00000000
+        
+        #Determine Packet type
+        #TODO: Add support for extension packets
+        if not context and not self.streamID:
+            hdr |= self.PKT_TYPE.if_data_packet_without_stream_id << 28
+        elif not context and self.streamID:
+            hdr |= self.PKT_TYPE.if_data_packet_with_stream_id << 28
+        elif context:
+            hdr |= self.PKT_TYPE.if_context_packet << 28
+        
+        #Set ClassID
+        hdr |= 0x1 << 27
+        
+        #TODO: Add trailer support
+        
+        #Set timestamp bits
+        #TODO: Add support for GPS time
+        if self.timestamp:
+            hdr |= self.TSI.utc_time << 22
+            hdr |= self.TSF.real_time << 20
+        else:
+            hdr |= self.TSI.no_timestamp << 22
+            hdr |= self.TSF.no_timestamp << 20
+        
+        #Set packet count
+        hdr |= (self.packet_count%16) << 16
+        self.packet_count += 1
+        
+        #Length gets set externally
+        
+        pkt.append(hdr)
+        
+        #Add Stream identifier
+        if self.streamID:
+            pkt.append(self.streamID)
+        
+        #Add ClassID
+        pkt.append(0x00FFFFFA) #Well Known first work
+        #2nd word is dependent on format
+        cid_word = 0x00000000
+        complex, width, type, cid_type, pack_type = self.format
+        if complex:
+            cid_word |= 1 << 20
+        cid_word |= cid_type << 16
+        cid_word |= self.vector_size-1
+        pkt.append(cid_word)
+        
+        #Add in timestamps
+        if self.timestamp:
+            frac, sec = math.modf(self.timestamp)
+            pkt.append(int(sec))
+            pkt.append(int(frac*1e12) >> 32)
+            pkt.append(int(frac*1e12) & 0xFFFFFFFF)
+        
+        return pkt
+    
+    def setPacketLength(self, pkt, length):
+        #Set length header field post-creation
+        if len(pkt) > 65536:
+            raise RuntimeError("Packet is too large")
+        pkt[0] |= length
+    
+    def toVITA49Int(self, value, radix=20):
+        shifted = int(value * (1 << radix) )
+        upper = (shifted & 0xFFFFFFFF00000000) >> 32
+        lower = shifted & 0xFFFFFFFF
+        return upper, lower
+    
+    def generateVRLFrame(self, data=None, force_send_context=True, packet_max_elements=None):
+        VRL_FAW = 0x56524C50
+        NO_CRC = 0x56454E44
+        
+        #Generate content
+        content = ""
+        if force_send_context:
+            content += self.generateContextPacket()
+        if data != None:
+            content += self.generateDataPacket(data)
+        
+        #Add header
+        frame = struct.pack('!I', VRL_FAW)
+        frame += struct.pack('!I', len(content)/4+3)
+        #Add content
+        frame += content
+        #Add No-CRC trailer
+        frame += struct.pack('!I', NO_CRC)
+        
+        return frame
+    
+    def generateContextPacket(self, allowed_cif_fields=CIF.all_fields):
+        #Generate header
+        pkt = self.generateCommonHeader(context=True)
+        
+        #Set to no fields
+        cif = self.CIF.no_fields
+        context_fields = []
+        
+        #Check each supported field if its set and allowed
+        if self.bandwidth and self.CIF.bandwidth & allowed_cif_fields != 0x0:
+            cif |= self.CIF.bandwidth
+            upper, lower = self.toVITA49Int(self.bandwidth)
+            context_fields.append(upper)
+            context_fields.append(lower)
+        
+        if self.if_ref and self.CIF.if_ref_freq & allowed_cif_fields != 0x0:
+            cif |= self.CIF.if_ref_freq
+            upper, lower = self.toVITA49Int(self.if_ref)
+            context_fields.append(upper)
+            context_fields.append(lower)
+        
+        if self.rf_ref and self.CIF.rf_ref_freq & allowed_cif_fields != 0x0:
+            cif |= self.CIF.rf_ref_freq
+            upper, lower = self.toVITA49Int(self.rf_ref)
+            context_fields.append(upper)
+            context_fields.append(lower)
+        
+        if self.sample_rate and self.CIF.sample_rate & allowed_cif_fields != 0x0:
+            cif |= self.CIF.sample_rate
+            upper, lower = self.toVITA49Int(self.sample_rate)
+            context_fields.append(upper)
+            context_fields.append(lower)
+        
+        #Always need a data format field
+        cif |= self.CIF.pkt_payload_fmt
+        complex, width, type, cid_type, pack_type = self.format
+        format_upper = 0x00000000
+        if complex:
+            format_upper |= 1 << 29
+        format_upper |= type << 24
+        
+        effective_width = width * 8 #Widths are in bits
+        format_upper |= (effective_width - 1) << 6  #These are one less than the width????
+        format_upper |= (effective_width - 1) #These are one less than the width????
+        format_lower = 0x00000000
+        format_lower |= self.vector_size - 1
+        context_fields.append(format_upper)
+        context_fields.append(format_lower)
+        
+        pkt.append(cif)
+        for field in context_fields:
+            pkt.append(field)
+        
+        self.setPacketLength(pkt, len(pkt))
+        return struct.pack('!%dI'%len(pkt), *pkt)
+    
+    def generateDataPacket(self, data):
+        pkt = self.generateCommonHeader()
+        
+        
+        content = ""
+        
+        #Flatten data presented as tuples
+        complex, width, v49_type, cid_type, pack_type = self.format
+        if complex and type(data[0]) == type(tuple()):
+            #Data presented as tuples, unwind it
+            flat_data = []
+            for d in data:
+                flat_data.append(d[0])
+                flat_data.append(d[1])
+        else:
+            flat_data = data
+        
+        #Verify that we have the amount of data as indicated in the header
+        if len(flat_data)/(1+complex) != self.vector_size:
+            raise RuntimeError("Must provide the same amount of data as indicated in VITA49.vector_size")
+        
+        #Verify we are hitting a word boundary
+        if len(flat_data)*width % 32 != 0:
+            raise RuntimeError("Must provide data that packs to a 32 bit boundary" )
+        
+        #Byte swap each element and append in
+        for d in flat_data:
+            content += struct.pack('!%s'%pack_type, d)
+        
+        self.setPacketLength(pkt, len(pkt) + len(content)/4)
+        return struct.pack('!%dI'%len(pkt), *pkt)+content
+    
+    def printData(self, pkt):
+        for i in range(0, len(pkt), 4):
+            print "{0:4} {1}".format(i, binascii.hexlify(pkt[i:i+4]))
+    
+    
+if __name__ == "__main__":
+    v49 = VITA49() 
+    v49.timestamp = time.time()
+    v49.streamID = 12345
+    v49.format = v49.DATA_FORMATS.SI
+    v49.bandwidth = 1.0e3
+    v49.sample_rate = 1.25e3
+    v49.vector_size = 256
+    v49.if_ref = 1.0e6
+    v49.rf_ref = 2.0e9
+    v49.vector_size = 256
+    
+    frame = v49.generateVRLFrame(None, force_send_context=True)
+    v49.printData(frame)
+    frame = v49.generateVRLFrame(xrange(0,256))
+    v49.printData(frame)
+    
+    
+    
+    
+    
